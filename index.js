e8b5d7b2878d6ef0549f3fa907f29b872042edba
Rafal Swietek <raf.swietek@gmail.com>
2020-10-14 23:38:28 +0200

    Updated configuration

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 module.exports = {
   extends: [
-    '@rafalswietek/eslint-config',
     'airbnb',
     'airbnb/hooks',
   ],
   parser: '@babel/eslint-parser',
   rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+        ],
+        allowAfterThis: true,
+        allowAfterSuper: true,
+      },
+    ],
+    'no-confusing-arrow': 'error',
+    'class-methods-use-this': 0,
+    'max-nested-callbacks': ['error', 3],
+    'max-classes-per-file': ['error', 1],
+    'no-else-return': 'error',
+    'max-params': ['error', 5],
+    'function-paren-newline': ['error', 'consistent'],
     complexity: ['error', 10],
     'max-depth': ['error', 5],
     'jsx-a11y/href-no-hash': 'off',
